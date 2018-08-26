425a092bc3bdd0262b298f7c3cecca7be5e55131
johnstoermer <john.stoermer@gmail.com>
2018-08-26 15:53:43 -0400

    Skips Erronous Pages, Added Timer
    Runs fast now using multiple threads, need to run it on a better computer though if I want to get through 8 million web pages in a day.

--- scraper.py
+++ scraper.py
@@ -6,7 +6,9 @@ import re
 import json
 from multiprocessing import Pool
 
-def addActor(actor_id):
+def addActor(actor_id): # TODO: Figure out why this is slower than addMovie (Average Speed ~0.13s)
+    start = time.time()
+    print(str(actor_id) + ' started')
     movies = []
     url = 'https://www.imdb.com/name/nm'
     #adding the actor_id to url
@@ -17,16 +19,22 @@ def addActor(actor_id):
     #
     page = requests.get(url)
     tree = html.fromstring(page.content)
-    name = tree.xpath('//*[@id="overview-top"]/h1/span[1]/text()')[0]
-    birthday = int(tree.xpath('//*[@id="name-born-info"]/time/a[2]/text()')[0])
-    movies = tree.xpath('//*[starts-with(@id, "actor-tt")]/b/a/text()')
-    dict = {}
-    dict['Birthday'] = birthday
-    dict['Movies'] = movies
-    return [dict, name]
+    try:
+        name = tree.xpath('//*[@id="overview-top"]/h1/span[1]/text()')[0]
+        birthday = int(tree.xpath('//*[@id="name-born-info"]/time/a[2]/text()')[0])
+        movies = tree.xpath('//*[starts-with(@id, "actor-tt")]/b/a/text()')
+        dict = {}
+        dict['Birthday'] = birthday
+        dict['Movies'] = movies
+        end = time.time()
+        print(str(actor_id) + ' ended at ' + str(end - start))
+        return [dict, name]
+    except:
+        return None
 
-def addMovie(movie_id):
+def addMovie(movie_id): #Average Speed ~0.04s
     start = time.time()
+    print(str(movie_id) + ' started')
     url = 'https://www.imdb.com/title/tt'
     #adding the movie_id to url
     zeroes = 6 - math.floor(math.log10(movie_id)) #finding the number of zeroes in the movie_id
@@ -35,37 +43,39 @@ def addMovie(movie_id):
     url += str(movie_id) + '/'
     #
     page = requests.get(url)
-    end = time.time()
-    print(str(end - start))
     tree = html.fromstring(page.content)
-    title = tree.xpath('//*[@id="title-overview-widget"]/div[2]/div[2]/div/div[2]/div[2]/h1/text()')[0].replace('\xa0', '')
-    print(title)
-    rating = float(tree.xpath('//*[@id="title-overview-widget"]/div[2]/div[2]/div/div[1]/div[1]/div[1]/strong/span/text()')[0])
-    reviews = int(tree.xpath('//*[@id="title-overview-widget"]/div[2]/div[2]/div/div[1]/div[1]/a/span/text()')[0].replace(',', ''))
-    date = int(tree.xpath('//*[@id="titleYear"]/a/text()')[0])
-    count = 2
-    cast = []
-    while True:
-        try:
-            cast.append(tree.xpath('//*[@id="titleCast"]/table/tr[{}]/td[2]/a/text()'.format(count))[0][1:-2]) #calling [0] on an empty array will cause an error and break
-            count += 1
-        except:
-            break
-    count = 1
-    genres = []
-    while True:
-        try:
-            genres.append(tree.xpath('//*[@id="titleStoryLine"]/div[3]/a[{}]/text()'.format(count))[0][1:]) #calling [0] on an empty array will cause an error and break
-            count += 1
-        except:
-            break
-    dict = {}
-    dict['Rating'] = rating
-    dict['Reviews'] = reviews
-    dict['Genres'] = genres
-    dict['Cast'] = cast
-    dict['Date'] = date
-    return [dict, title]
+    try:
+        title = tree.xpath('//*[@id="title-overview-widget"]/div[2]/div[2]/div/div[2]/div[2]/h1/text()')[0].replace('\xa0', '')
+        rating = float(tree.xpath('//*[@id="title-overview-widget"]/div[2]/div[2]/div/div[1]/div[1]/div[1]/strong/span/text()')[0])
+        reviews = int(tree.xpath('//*[@id="title-overview-widget"]/div[2]/div[2]/div/div[1]/div[1]/a/span/text()')[0].replace(',', ''))
+        date = int(tree.xpath('//*[@id="titleYear"]/a/text()')[0])
+        count = 2
+        cast = []
+        while True:
+            try:
+                cast.append(tree.xpath('//*[@id="titleCast"]/table/tr[{}]/td[2]/a/text()'.format(count))[0][1:-2]) #calling [0] on an empty array will cause an error and break
+                count += 1
+            except:
+                break
+        count = 1
+        genres = []
+        while True:
+            try:
+                genres.append(tree.xpath('//*[@id="titleStoryLine"]/div[3]/a[{}]/text()'.format(count))[0][1:]) #calling [0] on an empty array will cause an error and break
+                count += 1
+            except:
+                break
+        dict = {}
+        dict['Rating'] = rating
+        dict['Reviews'] = reviews
+        dict['Genres'] = genres
+        dict['Cast'] = cast
+        dict['Date'] = date
+        end = time.time()
+        print(str(movie_id) + ' ended at ' + str(end - start))
+        return [dict, title]
+    except:
+        return None
 
 
 def generateJSON(dict):
@@ -75,18 +85,26 @@ def generateJSON(dict):
 
 def main():
     dict = {'Actors': {}, 'Movies': {}}
-    with Pool(10) as p:
+    start = time.time()
+    with Pool(20) as p:
         d = p.map(addActor, range(1, 100))
         p.close()
         p.join()
         for a in d:
-            dict['Actors'][str(a[1])] = a[0]
-    with Pool(10) as p:
+            if a != None:
+                dict['Actors'][str(a[1])] = a[0]
+    end = time.time()
+    print('Average Time per Actor: ' + str((end - start)/99))
+    start = time.time()
+    with Pool(20) as p:
         d = p.map(addMovie, range(1, 100))
         p.close()
         p.join()
         for a in d:
-            dict['Movies'][str(a[1])] = a[0]
+            if a != None:
+                dict['Movies'][str(a[1])] = a[0]
+    end = time.time()
+    print('Average Time per Movie: ' + str((end - start)/99))
     generateJSON(dict)
 
 if __name__ == '__main__':
